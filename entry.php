f3554ecc1fb905b41a34c364e680816441b0b18e
zhang chu <zhtrue@gmail.com>
2015-03-10 15:39:03 +0800

    添加xhprof支持

--- entry.php
+++ entry.php
@@ -1,5 +1,7 @@
 <?php
 
+define('MAIN_DOMAIN', 'yuxiaoyan.cn');
+
 define('CODE_ROOT', '/usr/share/php/');
 define('COMMON_CLASS_PATH', CODE_ROOT.'apps/include/');
 define('COMMON_CONF_PATH', CODE_ROOT.'apps/conf/');
@@ -14,6 +16,10 @@ define('KO_DB_NAME', 'demo');
 define('KO_MC_HOST', 'e77874bc68b911e4.m.cnbjalicm12pub001.ocs.aliyuncs.com:11211');
 define('KO_SMARTY_INC', CODE_ROOT.'Smarty-3.1.21/libs/Smarty.class.php');
 define('KO_TEMPLATE_C_DIR', COMMON_RUNDATA_PATH.'templates_c/');
+define('KO_XHPROF', true);
+define('KO_XHPROF_LIBDIR', CODE_ROOT.'xhprof/xhprof_lib/');
+define('KO_XHPROF_WEBBASE', 'http://xhprof.'.MAIN_DOMAIN.':'.getenv('SERVER_PORT').'/xhprof_html/');
+define('KO_XHPROF_TMPDIR', COMMON_RUNDATA_PATH.'xhprof/');
 require_once(CODE_ROOT.'ko/ko.class.php');
 
 Ko_Web_Event::On('ko.bootstrap', 'before', function()
