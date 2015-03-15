b56ad002c80b8bb7ddb221879dd117fd3e49500e
zhang chu <zhtrue@gmail.com>
2015-03-15 22:11:21 +0800

    内容中心能自动转换dataurl

--- include/content/Api.php
+++ include/content/Api.php
@@ -12,4 +12,47 @@ class KContent_Api extends Ko_Mode_Content
 			),
 		),
 	);
+	
+	public function bSet($iAid, $iId, $sContent)
+	{
+		if (isset($this->_aConf['app'][$iAid]) && 'html' === $this->_aConf['app'][$iAid]['type'])
+		{
+			$sContent = $this->_sReplaceDataUrl($sContent, '"');
+			$sContent = $this->_sReplaceDataUrl($sContent, "'");
+		}
+		return parent::bSet($iAid, $iId, $sContent);
+	}
+	
+	private function _sReplaceDataUrl($sContent, $quote)
+	{
+		while (1)
+		{
+			$pos1 = stripos($sContent, 'src='.$quote.'data:');
+			if (false === $pos1)
+			{
+				break;
+			}
+			$pos2 = strpos($sContent, $quote, $pos1 + 10);
+			if (false !== $pos2)
+			{
+				$dataurl = substr($sContent, $pos1 + 5, $pos2 - $pos1 - 5);
+			}
+			else
+			{
+				$dataurl = substr($sContent, $pos1 + 5);
+			}
+			$data = file_get_contents($dataurl);
+			$api = new KStorage_Api;
+			if (false !== $data && $api->bContent2Storage($data, $sDest))
+			{
+				$url = $api->sGetUrl($sDest, 'imageView2/2/w/600/h/600');
+			}
+			else
+			{
+				$url = '';
+			}
+			$sContent = substr($sContent, 0, $pos1 + 5).$url.substr($sContent, $pos2);
+		}
+		return $sContent;
+	}
 }
