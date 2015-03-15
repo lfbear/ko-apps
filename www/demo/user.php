cd73b4fca9b3f5812369f4368befda8f8edc8207
zhang chu <zhtrue@gmail.com>
2015-03-15 20:05:54 +0800

    保存草稿

--- www/demo/user.php
+++ www/demo/user.php
@@ -8,6 +8,45 @@ Ko_Web_Route::VPost('logout', function()
 	Ko_Web_Response::VSend();
 });
 
+Ko_Web_Route::VPost('draft', function()
+{
+	$loginApi = new KUser_loginApi;
+	$uid = $loginApi->iGetLoginUid();
+	if ($uid)
+	{
+		$contentApi = new KContent_Api;
+		if ($contentApi->bSet(0, $uid, Ko_Web_Request::SPost('content')))
+		{
+			$data = array(
+				'errno' => 0,
+			);
+		}
+		else
+		{
+			$data = array(
+				'errno' => 2,
+				'error' => '保存失败',
+			);
+		}
+	}
+	else
+	{
+		$data = array(
+			'errno' => 1,
+			'error' => '请先登录',
+		);
+	}
+	$render = new Ko_View_Render_JSON;
+	$render->oSetData($data);
+	Ko_Web_Response::VAppendBody($render);
+	Ko_Web_Response::VSend();
+});
+
+
+
+
+
+
 Ko_Web_Route::VGet('login', function()
 {
 	$render = new KRender_default;
