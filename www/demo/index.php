8e1270dd6a897bcf65e52894369e0555cb763dc0
zhang chu <zhtrue@gmail.com>
2015-03-24 20:53:51 +0800

    优化流程

--- www/demo/index.php
+++ www/demo/index.php
@@ -6,8 +6,8 @@ $render->oSetTemplate('index.html');
 $loginApi = new KUser_loginApi;
 $uid = $loginApi->iGetLoginUid();
 $contentApi = new KContent_Api;
-if ($uid && '' !== trim($contentApi->sGetHtml(KContent_Api::USER_DRAFT, $uid)))
-{
+if ($uid && '' !== trim(Ko_Html_ImgParse::sParse($contentApi->sGetHtml(KContent_Api::USER_DRAFT, $uid))))
+{	//如果用户登录，并且有草稿
 	$htmlrender = new Ko_View_Render_HTML($contentApi);
 	$htmlrender->oSetData(KContent_Api::USER_DRAFT, $uid);
 	$render->oSetData('draft', $htmlrender);
@@ -26,3 +26,10 @@ else
 
 Ko_Web_Response::VAppendBody($render);
 Ko_Web_Response::VSend();
+
+if ($uid && $draftid)
+{	//用户登录，并且没有草稿，将未登录用户的草稿转移为登录用户的草稿
+	$draft = $contentApi->sGetHtml(KContent_Api::UUID_DRAFT, $draftid);
+	$contentApi->bSet(KContent_Api::USER_DRAFT, $uid, $draft);
+	$contentApi->bSet(KContent_Api::UUID_DRAFT, $draftid, '');
+}
