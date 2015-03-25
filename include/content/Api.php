504e838bcd4e16e32a0071ff3285c1dc98028bd6
zhang chu <zhtrue@gmail.com>
2015-03-25 19:55:19 +0800

    简化业务程序

--- include/content/Api.php
+++ include/content/Api.php
@@ -17,46 +17,13 @@ class KContent_Api extends Ko_Mode_Content
 		),
 	);
 	
-	public function bSet($iAid, $iId, $sContent)
+	protected function _sDataUrl2Link($sData)
 	{
-		if (isset($this->_aConf['app'][$iAid]) && 'html' === $this->_aConf['app'][$iAid]['type'])
+		$api = new KStorage_Api;
+		if ($api->bContent2Storage($sData, $sDest))
 		{
-			$sContent = $this->_sReplaceDataUrl($sContent, '"');
-			$sContent = $this->_sReplaceDataUrl($sContent, "'");
+			return $api->sGetUrl($sDest, 'imageView2/2/w/600/h/600');
 		}
-		return parent::bSet($iAid, $iId, $sContent);
-	}
-	
-	private function _sReplaceDataUrl($sContent, $quote)
-	{
-		while (1)
-		{
-			$pos1 = stripos($sContent, 'src='.$quote.'data:');
-			if (false === $pos1)
-			{
-				break;
-			}
-			$pos2 = strpos($sContent, $quote, $pos1 + 10);
-			if (false !== $pos2)
-			{
-				$dataurl = substr($sContent, $pos1 + 5, $pos2 - $pos1 - 5);
-			}
-			else
-			{
-				$dataurl = substr($sContent, $pos1 + 5);
-			}
-			$data = file_get_contents($dataurl);
-			$api = new KStorage_Api;
-			if (false !== $data && $api->bContent2Storage($data, $sDest))
-			{
-				$url = $api->sGetUrl($sDest, 'imageView2/2/w/600/h/600');
-			}
-			else
-			{
-				$url = '';
-			}
-			$sContent = substr($sContent, 0, $pos1 + 5).$url.substr($sContent, $pos2);
-		}
-		return $sContent;
+		return parent::_sDataUrl2Link($sData);
 	}
 }
