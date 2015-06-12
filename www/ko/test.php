2d1c136b6c761ee0a08df2d4834056ccab578339
zhangchu <zhtrue@gmail.com>
2015-06-12 16:53:04 +0800

    test

--- www/ko/test.php
+++ www/ko/test.php
@@ -15,3 +15,27 @@ Ko_Web_Route::VGet('tinymce', function()
 	Ko_Web_Response::VAppendBody($render);
 	Ko_Web_Response::VSend();
 });
+
+Ko_Web_Route::VGet('push', function()
+{
+	Ko_Web_Response::VSetContentType('text/event-stream');
+	Ko_Web_Response::VSend();
+	for ($i=0; $i<2; $i++)
+	{
+		echo 'data:'.date('Y-m-d H:i:s')."\r\n";
+		echo 'id:'.(1-$i)."\r\n";
+		echo "\r\n";
+		flush();
+		sleep(1);
+	}
+	echo 'retry: 5000'."\r\n";
+	echo "\r\n";
+});
+
+Ko_Web_Route::VGet('index', function()
+{
+	$render = new KRender_default;
+	$render->oSetTemplate('ko/test/index.html');
+	Ko_Web_Response::VAppendBody($render);
+	Ko_Web_Response::VSend();
+});
