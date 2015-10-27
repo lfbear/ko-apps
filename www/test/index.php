810c65100adf5642f48da0be44bd85eb8de1ccc1
zhangchu <zhtrue@gmail.com>
2015-10-27 17:28:20 +0800

    debug

--- www/test/index.php
+++ www/test/index.php
@@ -1,63 +1,11 @@
 <!doctype html>
-<html ng-app="test">
+<html>
 <head>
-	<script src="http://img.zhangchu.cc/js/angular-1.4.5/angular.min.js"></script>
-	<script>
-		angular.module('test', []).controller('PhoneListCtrl', function($scope) {
-			$scope.phones = [
-				{'name': 'Nexus S',
-					'snippet': 'Fast just got faster with Nexus S.',
-					'age': 1},
-				{'name': 'Motorola XOOM™ with Wi-Fi',
-					'snippet': 'The Next, Next Generation tablet.',
-					'age': 2},
-				{'name': 'MOTOROLA XOOM™',
-					'snippet': 'The Next, Next Generation tablet.',
-					'age': 3}
-			];
-
-			$scope.orderProp = 'age';
-			$scope.hello = "Hello, World!";
-		});
-	</script>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width,minimum-scale=0.1,maximum-scale=10">
+	<link href="http://img.zhangchu.cc/css/reset.css" rel="stylesheet">
 </head>
-<body ng-controller="PhoneListCtrl">
-Your name: <input type="text" ng-model="yourname" placeholder="World">
-<hr>
-Hello {{yourname || 'World'}}!
-
-<p>1 + 2 = {{ 1 + 2 }}</p>
-
-<p>Total number of phones: {{phones.length}}</p>
-
-<p>{{hello}}</p>
-
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-2">
-			<!--Sidebar content-->
-
-			Search: <input ng-model="query">
-			Sort by:
-			<select ng-model="orderProp">
-				<option value="name">Alphabetical</option>
-				<option value="age">Newest</option>
-			</select>
-
-		</div>
-		<div class="col-md-10">
-			<!--Body content-->
-
-			<ul class="phones">
-				<li ng-repeat="phone in phones | filter:query | orderBy:orderProp">
-					{{phone.name}} {{phone.age}}
-					<p>{{phone.snippet}}</p>
-				</li>
-			</ul>
-
-		</div>
-	</div>
-</div>
-
+<body>
+<img src="http://7xawfx.com1.z0.glb.clouddn.com/Fn4VeEKcTzlutRym1FNd5Z8iQndv?imageView2/0/w/800">
 </body>
 </html>
